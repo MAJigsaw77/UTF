fdabec1e1648768662ac2dabaa5e7adfb693133c
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-07 19:39:32 +0300

    Update MainState.hx

--- src/MainState.hx
+++ src/MainState.hx
@@ -10,7 +10,7 @@ class PlayState extends FlxState
         super.create();
 
         var text:FlxText = new FlxText(0, 0, 0, 'wanna have a\nbad time?', 32);
-        text.font = 'assets/fonts/DTM-Sans.ttf';
+        text.font = 'assets/fonts/DTM-Sans.otf';
         text.screenCenter();
         add(text);
     }
