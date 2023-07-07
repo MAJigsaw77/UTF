12bc7d9fad7fe98a197a4dedcb17ded95f99106c
MAJigsaw77 <mihaialexandru427@yahoo.com>
2023-07-08 00:03:07 +0300

    assets

--- src/MainState.hx
+++ src/MainState.hx
@@ -19,4 +19,19 @@ class MainState extends FlxState
     {
         super.update(elapsed);
     }
+
+	private function createDBGraphic():BitmapData
+	{
+		var shape:Shape = new Shape();
+		shape.graphics.beginFill(0xFFFFFFFF);
+		shape.graphics.drawRect(0, 0, 304, 235);
+		shape.graphics.endFill();
+		shape.graphics.beginFill(0);
+		shape.graphics.drawRect(3, 3, 301, 232);
+		shape.graphics.endFill();
+
+		var bitmap:BitmapData = new BitmapData(304, 235, true, 0);
+		bitmap.draw(shape, true);
+		return bitmap;
+	}
 }
