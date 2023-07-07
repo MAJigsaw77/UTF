8297ef02ebc8fec4a0cdc06de9d89cf83e8e3a4a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-07 19:25:30 +0300

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -17,21 +17,18 @@ class Main extends Sprite
 		FlxG.signals.preStateCreate.add(function(state:FlxState)
 		{
 			// Clear the loaded graphics if they are no longer in flixel cache...
-			for (key => value in Assets.cache.bitmapData)
+			for (key in Assets.cache.bitmapData.keys())
 				if (!FlxG.bitmap.checkCache(key))
 					Assets.cache.removeBitmapData(key);
-
-			// Clear the loaded songs as they use the most memory...
-			Assets.cache.clear(getPath('songs', 'songs'));
 
 			// Run the garbage colector...
 			System.gc();
 		});
 		FlxG.signals.postStateCreate.add(System.gc);
 
-		addChild(new FlxGame(1280, 720, MainState, 60, 60));
+		addChild(new FlxGame(640, 480, MainState, 60, 60));
 
-		var fpsCounter:FPS = new FPS(10, 3, 0xFFFFFF);
+		var fpsCounter:FPS = new FPS(10, 10, 0xFFFFFF);
 		fpsCounter.showMemory = #if debug true #else false #end;
 		addChild(fpsCounter);
 	}
