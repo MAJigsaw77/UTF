20d2e9a933a474560c85bc5a308d802ee0040a45
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-09 08:46:33 +0300

    Update BattleState.hx

--- src/BattleState.hx
+++ src/BattleState.hx
@@ -59,12 +59,12 @@ class BattleState extends FlxState
         hpBar.scrollFactor.set();
         add(hpBar);
 
-        hpInfo = new FlxText(290 + Global.maxhp * 1.2, 400, 0, Global.hp + ' / ' + Global.maxhp, 12);
+        hpInfo = new FlxText(290 + Global.maxhp * 1.2, 400, 0, Global.hp + ' / ' + Global.maxhp, 18);
         hpInfo.font = Paths.font('Small.otf');
         hpInfo.scrollFactor.set();
         add(hpInfo);
 
-        stats = new FlxText(30, 400, 0, Global.charname + "   LV " + Global.lv, 12);
+        stats = new FlxText(30, 400, 0, Global.charname + "   LV " + Global.lv, 18);
         stats.font = Paths.font('Small.otf');
         stats.scrollFactor.set();
         add(stats);
