ce874fa749acb03715125236b368d9eff26ce97f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-15 22:00:10 +0300

    Update Monster.hx

--- src/Monster.hx
+++ src/Monster.hx
@@ -1,3 +1,18 @@
 package;
 
-class Monster extends FlxSpriteGroup {}
+import flixel.group.FlxSpriteGroup;
+import haxe.Json;
+import openfl.utils.Assets;
+
+class Monster extends FlxSpriteGroup
+{
+	public function new():Void
+	{
+		super();
+	}
+
+	public override function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
