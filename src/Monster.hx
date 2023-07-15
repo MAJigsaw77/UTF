4117f66b415c052929dd4eeed00cfd5f2f234929
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-15 22:03:55 +0300

    Update Monster.hx

--- src/Monster.hx
+++ src/Monster.hx
@@ -4,6 +4,10 @@ import flixel.group.FlxSpriteGroup;
 import haxe.Json;
 import openfl.utils.Assets;
 
+typedef MonsterData = {
+	var name:String;
+}
+
 class Monster extends FlxSpriteGroup
 {
 	public function new():Void
