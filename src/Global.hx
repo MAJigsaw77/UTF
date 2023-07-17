90cbac85ffd0a695cec391f0987a4860bb982fd3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-17 18:48:56 +0300

    Update Global.hx

--- src/Global.hx
+++ src/Global.hx
@@ -1,12 +1,24 @@
 package;
 
+typedef Weapon = {
+	var name:String;
+	var attack:Float;
+	var description:String;
+}
+
+typedef Armor = {
+	var name:String;
+	var defense:Float;
+	var description:String;
+}
+
 class Global
 {
 	public static var name:String = 'CHARA';
 	public static var hp:Int = 20;
 	public static var maxhp:Int = 20;
-	public static var attack:Int = 10;
-	public static var defense:Int = 10;
+	public static var attack:Float = 10;
+	public static var defense:Float = 10;
 	public static var gold:Int = 0;
 	public static var xp:Int = 0;
 	public static var lv:Int = 1;
