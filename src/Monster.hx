ab4ff5ce0b0c6470e23383fa5e568e8ee69a333b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-17 18:50:14 +0300

    Update Monster.hx

--- src/Monster.hx
+++ src/Monster.hx
@@ -4,16 +4,6 @@ import flixel.group.FlxSpriteGroup;
 import haxe.Json;
 import openfl.utils.Assets;
 
-typedef MonsterData = {
-	var name:String;
-	var hp:Int;
-	var maxHp:Int;
-	var attack:Float;
-	var defense:Float;
-	var xpReward:Int;
-	var goldReward:Int;
-}
-
 class Monster extends FlxSpriteGroup
 {
 	public var data(default, null):MonsterData;
