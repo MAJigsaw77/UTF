552ff84838635efe45c5a97f4cde75c2e1224fd3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-17 21:00:51 +0300

    Update Global.hx

--- src/Global.hx
+++ src/Global.hx
@@ -31,7 +31,7 @@ class Global
 {
 	public static var name:String = 'CHARA';
 	public static var hp:Int = 20;
-	public static var maxhp:Int = 20;
+	public static var maxHp:Int = 20;
 	public static var attack:Float = 10;
 	public static var defense:Float = 10;
 	public static var gold:Int = 0;
@@ -41,40 +41,66 @@ class Global
 
 	public static function levelUp():Bool
 	{
-		var currentLevel:Int = Global.lv;
-/*if(global.xp >= 10) global.lv= 2;
-if(global.xp >= 30) global.lv= 3;
-if(global.xp >= 70) global.lv= 4;
-if(global.xp >= 120) global.lv= 5;
-if(global.xp >= 200) global.lv= 6;
-if(global.xp >= 300) global.lv= 7;
-if(global.xp >= 500) global.lv= 8;
-if(global.xp >= 800) global.lv= 9;
-if(global.xp >= 1200) global.lv= 10;
-if(global.xp >= 1700) global.lv= 11;
-if(global.xp >= 2500) global.lv= 12;
-if(global.xp >= 3500) global.lv= 13;
-if(global.xp >= 5000) global.lv= 14;
-if(global.xp >= 7000) global.lv= 15;
-if(global.xp >= 10000) global.lv= 16;
-if(global.xp >= 15000) global.lv= 17;
-if(global.xp >= 25000) global.lv= 18;
-if(global.xp >= 50000) global.lv= 19;
-if(global.xp >= 99999) {
-    global.lv= 20;
-    global.xp= 99999;
-}
-if(global.lv != currentlevel) {
-    levelup= 1;
-    global.maxhp= 16 + global.lv * 4;
-    global.at= 8 + global.lv * 2;
-    global.df= 9 + ceil(global.lv / 4);
-    if(global.lv == 20) {
-        global.maxhp= 99;
-        global.at= 99;
-        global.df= 99;
-    }
-} else  levelup= 0;*/
+		final currentLevel:Int = Global.lv;
+
+		switch (Global.xp)
+		{
+			case value if (value >= 99999)
+				Global.lv = 20;
+				Global.xp = 99999;
+			case value if (value >= 50000)
+				Global.lv = 19;
+			case value if (value >= 25000)
+				Global.lv = 18;
+			case value if (value >= 15000)
+				Global.lv = 17;
+			case value if (value >= 10000)
+				Global.lv = 16;
+			case value if (value >= 7000)
+				Global.lv = 15;
+			case value if (value >= 5000)
+				Global.lv = 14;
+			case value if (value >= 3500)
+				Global.lv = 13;
+			case value if (value >= 2500)
+				Global.lv = 12;
+			case value if (value >= 1700)
+				Global.lv = 11;
+			case value if (value >= 1200)
+				Global.lv = 10;
+			case value if (value >= 800)
+				Global.lv = 9;
+			case value if (value >= 500)
+				Global.lv = 8;
+			case value if (value >= 300)
+				Global.lv = 7;
+			case value if (value >= 200)
+				Global.lv = 6;
+			case value if (value >= 120)
+				Global.lv = 5;
+			case value if (value >= 70)
+				Global.lv = 4;
+			case value if (value >= 30)
+				Global.lv = 3;
+			case value if (value >= 10)
+				Global.lv = 2;
+		}
+				
+		if (Global.xp != currentlevel)
+		{
+			Global.maxHp = 16 + Global.lv * 4;
+			Global.attack = 8 + Global.lv * 2;
+			Global.defense = 9 + Math.ceil(Global.lv / 4);
+
+			if (Global.lv == 20)
+			{
+				Global.maxHp = 99;
+				Global.attack = 99;
+				Global.defense = 99;
+			}
+
+			return true;
+		}
 
 		return false;
 	}
