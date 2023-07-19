ca3a14625eebb402a9abb160c149d89fe35d81c4
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-19 21:23:15 +0300

    Update Startup.hx

--- src/Startup.hx
+++ src/Startup.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
 import flixel.FlxG;
 import flixel.FlxState;
 
@@ -7,6 +9,14 @@ class Startup
 {
 	override function create():Void
 	{
+		Global.load();
+		Mods.load();
+
+		FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 0.5);
+		FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 0.5);
+
+		super.create();
+
 		FlxG.switchState(new BattleState());
 	}
 }
