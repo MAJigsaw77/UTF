a8e9699761cd7cf84fad94d0eabf98c56564d7ab
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-19 13:32:50 +0300

    Update Mods.hx

--- src/Mods.hx
+++ src/Mods.hx
@@ -1,12 +1,15 @@
 package;
 
+#if android
+import android.content.Context;
+#end
 import flixel.FlxG;
 import openfl.Lib;
 import polymod.Polymod;
 
 class Mods
 {
-	private static var MOD_DIR(default, null):String = 'mods';
+	public static var MOD_DIR(default, null):String = #if android Context.getExternalStorageDirectory(null) + #end 'mods';
 
 	public static function load():Void
 	{
