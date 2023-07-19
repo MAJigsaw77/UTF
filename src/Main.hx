ab025626dd0c7d6e21e8dd752c7b3c2fe7a36af4
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-19 13:35:41 +0300

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -39,8 +39,10 @@ class Main extends Sprite
 		});
 		FlxG.signals.postStateSwitch.add(System.gc);
 
-		// FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 0.5);
-		// FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 0.5);
+		Mods.load();
+
+		FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 0.5);
+		FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 0.5);
 
 		addChild(new FlxGame(640, 480, BattleState, 30, 30, false, false));
 		addChild(new FPS(10, 10, FlxColor.WHITE));
