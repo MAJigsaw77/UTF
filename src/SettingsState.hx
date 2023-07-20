046ac9bd919cd79ff3b40b0bcea53cbb7f218af8
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-20 22:21:43 +0300

    Update SettingsState.hx

--- src/SettingsState.hx
+++ src/SettingsState.hx
@@ -6,9 +6,15 @@ class SettingsState extends FlxTransitionableState
 {
 	override function create():Void
 	{
-		switch (Date.now().getMonths() + 1)
+		switch (Date.now().getMonth() + 1)
 		{
-			
+			case 12 | 1 | 2: // Winter
+
+			case 3 | 4 | 5: // Spring
+
+			case 6 | 7 | 8: // Summer
+
+			case 9 | 10 | 11: // Autumn
 		}
 	}
 }
