9037d45a60558456488f0cdcc253102ae8370f09
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-20 22:54:19 +0300

    Update AssetPaths.hx

--- src/AssetPaths.hx
+++ src/AssetPaths.hx
@@ -1,27 +1,39 @@
 package;
 
-import openfl.utils.Assets;
-
 class AssetPaths
 {
 	public static inline function data(key:String):String
+	{
 		return 'assets/data/$key.json';
+	}
 
 	public static inline function script(key:String):String
+	{
 		return 'assets/data/$key.hxs';
+	}
 
 	public static inline function sprite(key:String):String
+	{
 		return 'assets/images/$key.png';
+	}
 
 	public static inline function sound(key:String):String
+	{
 		return 'assets/sounds/$key.wav';
+	}
 
 	public static inline function music(key:String):String
+	{
 		return 'assets/music/$key.ogg';
+	}
 
 	public static inline function shader(key:String):String
+	{
 		return 'assets/shaders/$key';
+	}
 
 	public static inline function font(key:String):String
+	{
 		return 'assets/fonts/$key';
+	}
 }
