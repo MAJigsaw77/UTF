59aced61049190c8f886b218c1d9f968f1dc6a97
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-20 22:38:51 +0300

    Update SettingsState.hx

--- src/SettingsState.hx
+++ src/SettingsState.hx
@@ -9,12 +9,13 @@ class SettingsState extends FlxTransitionableState
 		switch (Date.now().getMonth() + 1)
 		{
 			case 12 | 1 | 2: // Winter
-
+				FlxG.sound.playMusic(AssetPaths.music('options/winter'));
 			case 3 | 4 | 5: // Spring
-
+				FlxG.sound.playMusic(AssetPaths.music('options/fall'));
 			case 6 | 7 | 8: // Summer
-
+				FlxG.sound.playMusic(AssetPaths.music('options/summer'));
 			case 9 | 10 | 11: // Autumn
+				FlxG.sound.playMusic(AssetPaths.music('options/fall'));
 		}
 	}
 }
