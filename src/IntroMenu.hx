12e455d1fe2efb0fa412c4274a0bad51e6db3374
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-25 22:30:59 +0300

    Update IntroMenu.hx

--- src/IntroMenu.hx
+++ src/IntroMenu.hx
@@ -11,12 +11,12 @@ class IntroMenu extends FlxState
 
 	override function create():Void
 	{
-		bg = new FlxSprite(0, -304, AssetPaths.background('floweyglow'));
+		bg = new FlxSprite(0, -240, AssetPaths.background('floweyglow'));
 		bg.scale.set(2, 2);
 		bg.updateHitbox();
 		add(bg);
 
-		flowey = new FlxSprite(332, 402);
+		flowey = new FlxSprite(294, 338);
 		flowey.frames = AssetPaths.spritesheet('flowey', [1]);
 		flowey.scale.set(2, 2);
 		flowey.updateHitbox();
