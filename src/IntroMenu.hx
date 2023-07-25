43a38e04ddb913964c13a336de463d2c515e1a7b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-25 16:26:53 +0300

    Create IntroMenu.hx

---
+++ src/IntroMenu.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class IntroMenu extends FlxState
+{
+	override function create():Void
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
