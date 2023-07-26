07393a8e6e7a1a81e46f83ed752fcf6d3c0e0921
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-26 14:38:47 +0300

    Update IntroMenu.hx

--- src/IntroMenu.hx
+++ src/IntroMenu.hx
@@ -1,6 +1,8 @@
 package;
 
 import flixel.group.FlxGroup;
+import flixel.math.FlxMath;
+import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -11,24 +13,34 @@ class IntroMenu extends FlxState
 	final choices:Array<String> = ['Continue', 'Reset', 'Settings'];
 	var choicesItems:FlxTypedGroup<FlxSprite>;
 
-	var bg:FlxSprite;
-	var flowey:FlxSprite;
-
 	override function create():Void
 	{
-		bg = new FlxSprite(0, -240, AssetPaths.background('floweyglow'));
+		FlxG.sound.playMusic(AssetPaths.music('menu1'));
+
+		var bg:FlxSprite = new FlxSprite(0, -240, AssetPaths.background('floweyglow'));
 		bg.scale.set(2, 2);
 		bg.updateHitbox();
 		bg.scrollFactor.set();
 		add(bg);
 
-		flowey = new FlxSprite(294, 338);
+		var flowey:FlxSprite = new FlxSprite(0, 338);
 		flowey.frames = AssetPaths.spritesheet('flowey', [1]);
 		flowey.scale.set(2, 2);
 		flowey.updateHitbox();
 		flowey.screenCenter(X);
 		flowey.scrollFactor.set();
 		add(flowey);
+
+		for (i in 0...options.length)
+		{
+			var opt:FlxText = new FlxText(150 * i, 220, 0, options[i].toUpperCase(), 24);
+			opt.font = AssetPaths.font('DTM-Mono.otf');
+			opt.ID = i;
+			opt.scrollFactor.set();
+			optionsItems.add(opt);
+		}
+
+		changeOption();
 		
 		super.create();
 	}
@@ -39,5 +51,43 @@ class IntroMenu extends FlxState
 			FlxG.switchState(new Battle());
 
 		super.update(elapsed);
+	}
+
+	override function update(elapsed:Float):Void
+	{
+		if (FlxG.keys.justPressed.RIGHT)
+			changeOption(1);
+		else if (FlxG.keys.justPressed.LEFT)
+			changeOption(-1);
+		else if (FlxG.keys.anyJustPressed(Global.binds.get('continue')))
+		{
+			if (choices[curOption] != 'Reset')
+				FlxG.sound.music.stop();
+
+			switch (choices[curOption])
+			{
+				case 'Continue':
+					FlxG.switchState(new Battle());
+				case 'Reset':
+					// TODO
+				case 'Settings':
+					FlxG.switchState(new Settings());
+			}
+		}
+
+		super.update(elapsed);
+	}
+
+	private function changeOption(num:Int = 0):Void
+	{
+		curOption = FlxMath.wrap(curOption + num, 0, options.length - 1);
+
+		optionsItems.forEach(function(spr:FlxText)
+		{
+			if (spr.ID == curOption)
+				spr.color = FlxColor.YELLOW;
+			else
+				spr.color = FlxColor.WHITE;
+		});
 	}
 }
