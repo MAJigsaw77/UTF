3fc8a7d5a96ccba1720cc653bc76640d88291d5d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-29 16:23:45 +0300

    Update Macros.hx

--- src/Macros.hx
+++ src/Macros.hx
@@ -1,10 +1,11 @@
 package;
 
 import haxe.macro.Context;
+import haxe.macro.Expr;
 
 class Macros
 {
-        public static macro function getDefines() 
+        public static macro function getDefines():Expr
         {
                 return macro $v{Context.getDefines()};
         }
