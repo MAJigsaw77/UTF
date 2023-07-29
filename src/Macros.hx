6d4c08ed81834265684b782963878f15933c3b10
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-29 17:46:10 +0300

    Update Macros.hx

--- src/Macros.hx
+++ src/Macros.hx
@@ -2,9 +2,36 @@ package;
 
 import haxe.macro.Context;
 import haxe.macro.Expr;
+import sys.io.Process;
 
 class Macros
 {
+ 	public static macro function getCommitHash():Expr
+        {
+		try
+                {
+			var proc = new Process('git', ['rev-parse', '--short', 'HEAD'], false);
+			proc.exitCode(true);
+			return macro $v{proc.stdout.readLine()};
+		}
+                catch(e:Dynamic) {}
+
+		return macro $v{"-"};
+	}
+
+	public static macro function getCommitNumber():Expr
+        {
+		try
+                {
+			var proc = new Process('git', ['rev-list', 'HEAD', '--count'], false);
+			proc.exitCode(true);
+			return macro $v{Std.parseInt(proc.stdout.readLine())};
+		}
+                catch(e:Dynamic) {}
+
+		return macro $v{0};
+        }
+
         public static macro function getDefines():Expr
         {
                 return macro $v{Context.getDefines()};
