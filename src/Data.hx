1721ca4c04b63df66344dade0090ca3901bcaced
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-30 02:02:08 +0300

    Create Data.hx

---
+++ src/Data.hx
@@ -0,0 +1,37 @@
+package;
+
+import flixel.input.keyboard.FlxKey;
+import flixel.util.FlxSave;
+import openfl.Lib;
+
+class Data
+{
+	public static var settings:Map<String, Dynamic> = ['fps' => false];
+	public static var binds:Map<String, Array<FlxKey>> = ['confirm' => [Z, ENTER], 'cancel' => [X, SHIFT], 'menu' => [C, CONTROL]];
+
+	public static function save():Void
+	{
+		var save:FlxSave = new FlxSave();
+
+                @:privateAccess
+		save.bind('data', FlxSave.validate(Lib.application.meta.get('file')));
+
+		save.data.settings = settings;
+		save.data.binds = binds;
+                save.flush();
+	}
+
+	public static function load():Void
+	{
+		var save:FlxSave = new FlxSave();
+
+                @:privateAccess
+		save.bind('data', FlxSave.validate(Lib.application.meta.get('file')));
+
+		if (save.data.settings != null)
+                        binds = save.data.settings;
+
+                if (save.data.binds != null)
+                        binds = save.data.binds;
+	}
+}
