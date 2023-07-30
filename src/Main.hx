91a5fca5090e70584cbda34c434fc1197c905883
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-30 11:14:06 +0300

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -57,9 +57,9 @@ class Main extends Sprite
 
 		addChild(new FlxGame(640, 480, Startup, 30, 30, false, false));
 
-		fps = new FPS(10, 10, FlxColor.WHITE)
+		fps = new FPS(10, 10, FlxColor.WHITE);
 		fps.visible = Data.settings.get('fps');
-		addChild(new FPS(10, 10, FlxColor.WHITE));
+		addChild(fps);
 	}
 
 	private function onResizeGame(width:Int, height:Int):Void
