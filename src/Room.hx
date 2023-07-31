985e5a631fda7143ffd7282f564245b29466e82b
NeeEoo <NeeEoo@users.noreply.github.com>
2023-07-31 20:52:42 +0200

    Converted rooms to xml

--- src/Room.hx
+++ src/Room.hx
@@ -1,5 +1,7 @@
 package;
 
+import Xml;
+import haxe.xml.Access;
 import flixel.addons.transition.FlxTransitionableState;
 import flixel.FlxG;
 import flixel.FlxSprite;
@@ -24,35 +26,51 @@ typedef ObjectData =
 
 class Room extends FlxTransitionableState
 {
-	var data:RoomData = {id: 0, objects: []};
-	
+	var data:Xml = Xml.parse('<room id="0"></room>').firstElement();
+	var objects:Array<Dynamic> = [];
+
 	public function new(room:Int):Void
 	{
 		super();
 
+		var didLoad = false;
+
 		for (file in Assets.list(TEXT).filter(folder -> folder.startsWith('assets/data/rooms')))
 		{
 			// Being sure about something...
-			if (Path.extension(file) == 'json')
+			if (Path.extension(file) == 'xml')
 			{
-				var roomData:RoomData = Json.parse(Assets.getText(file));
-				if (roomData.id == room)
-					data = roomData;
+				var roomData:String = (Assets.getText(file));
+				data = Xml.parse(roomData).firstElement();
+				if (Std.parseInt(data.get("id")) == room) {
+					var access = new Access(data);
+					for(element in access.nodes.obj) {
+						objects.push({
+							spr: element.att.spr,
+							x: Std.parseFloat(element.att.x),
+							y: Std.parseFloat(element.att.y),
+							sx: element.has.sx ? Std.parseFloat(element.att.sx) : 1.0,
+							sy: element.has.sy ? Std.parseFloat(element.att.sy) : 1.0,
+						});
+					}
+					didLoad = true;
+					break;
+				}
 			}
 		}
 	}
 
 	override function create():Void
 	{
-		for (object in data.objects)
+		for (object in objects)
 		{
-			var obj:FlxSprite = new FlxSprite(object.position[0], object.position[1], AssetPaths.sprite(object.name));
-			obj.scale.set(object.scale[0], object.scale[1]);
+			var obj:FlxSprite = new FlxSprite(object.x, object.y, AssetPaths.sprite(object.spr));
+			obj.scale.set(object.sx, object.sy);
 			obj.updateHitbox();
 			obj.scrollFactor.set();
 			add(obj);
 		}
-		
+
 		super.create();
 	}
 }
