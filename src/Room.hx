ddd5e410df8feb765b85da01771dc7b6235622c6
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-31 22:08:02 +0300

    Cleanup

--- src/Room.hx
+++ src/Room.hx
@@ -1,50 +1,45 @@
 package;
 
-import Xml;
-import haxe.xml.Access;
 import flixel.addons.transition.FlxTransitionableState;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import haxe.io.Path;
-import haxe.Json;
+import haxe.xml.Access;
 import openfl.utils.Assets;
 
 using StringTools;
 
-typedef RoomData =
-{
-	id:Int,
-	objects:Array<ObjectData>
-}
-
 typedef ObjectData =
 {
-	name:String,
-	position:Array<Float>,
-	scale:Array<Float>
+	spr:String,
+	x:Float,
+	y:Float,
+	sx:Float,
+	sy:Float
 }
 
 class Room extends FlxTransitionableState
 {
 	var data:Xml = Xml.parse('<room id="0"></room>').firstElement();
-	var objects:Array<Dynamic> = [];
+	var objects:Array<ObjectData> = [];
 
 	public function new(room:Int):Void
 	{
 		super();
-
-		var didLoad = false;
 
 		for (file in Assets.list(TEXT).filter(folder -> folder.startsWith('assets/data/rooms')))
 		{
 			// Being sure about something...
 			if (Path.extension(file) == 'xml')
 			{
-				var roomData:String = (Assets.getText(file));
-				data = Xml.parse(roomData).firstElement();
-				if (Std.parseInt(data.get("id")) == room) {
+				data = Xml.parse(Assets.getText(file)).firstElement();
+
+				if (Std.parseInt(data.get("id")) == room)
+				{
 					var access = new Access(data);
-					for(element in access.nodes.obj) {
+
+					for (element in access.nodes.obj)
+					{
 						objects.push({
 							spr: element.att.spr,
 							x: Std.parseFloat(element.att.x),
@@ -53,7 +48,7 @@ class Room extends FlxTransitionableState
 							sy: element.has.sy ? Std.parseFloat(element.att.sy) : 1.0,
 						});
 					}
-					didLoad = true;
+	
 					break;
 				}
 			}
