4629011393b2e7818f3c33f6eb5fdb0b99e790ff
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-07-31 20:58:59 +0300

    Update Room.hx

--- src/Room.hx
+++ src/Room.hx
@@ -1,6 +1,13 @@
 package;
 
 import flixel.addons.transition.FlxTransitionableState;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import haxe.io.Path;
+import haxe.Json;
+import openfl.utils.Assets;
+
+using StringTools;
 
 typedef RoomData =
 {
@@ -17,15 +24,41 @@ typedef ObjectData =
 
 class Room extends FlxTransitionableState
 {
-	public function new(room:Int):Void {}
+	var data:RoomData;
+	
+	public function new(room:Int):Void
+	{
+		for (file in Assets.list(TEXT).filter(folder -> folder.startsWith('assets/data/rooms')))
+		{
+			// Being sure about something...
+			if (Path.extension(file) == 'json')
+			{
+				var roomData:RoomData = Json.parse(Assets.getText(file));
+				if (roomData.id == room)
+					data = roomData;
+			}
+		}
+
+		if (data == null)
+		{
+			data = {
+				id: room,
+				objects: []
+			};
+		}
+	}
 
 	override function create():Void
 	{
+		for (object in data.objects)
+		{
+			var obj:FlxSprite = new FlxSprite(object.position[0], object.position[1], AssetPaths.sprite(object.name));
+			obj.scale.set(object.scale[0], object.scale[1]);
+			obj.updateHitbox();
+			obj.scrollFactor.set();
+			add(obj);
+		}
+		
 		super.create();
-	}
-
-	override function update(elapsed:Float):Void
-	{
-		super.update(elapsed);
 	}
 }
