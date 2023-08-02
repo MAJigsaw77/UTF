75c178d4f480a05096c456d0ecef87d41bcab2b9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-08-02 04:22:50 +0300

    Update Room.hx

--- src/Room.hx
+++ src/Room.hx
@@ -51,7 +51,7 @@ class Room extends FlxTransitionableState
 					chara.scale.set(obj.has.scaleX ? Std.parseFloat(obj.att.scaleX) : 1.0, obj.has.scaleY ? Std.parseFloat(obj.att.scaleY) : 1.0);
 					chara.updateHitbox();
 					chara.scrollFactor.set();
-					insert(members.indexOf(objects), chara);
+					add(chara);
 				default:
 					var object:FlxSprite = new FlxSprite(Std.parseFloat(obj.att.x), Std.parseFloat(obj.att.y), AssetPaths.sprite(obj.att.name));
 
