5a6b2bc1cc55c6cfa2ab693af5016930602e005c
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-08-03 15:48:16 +0300

    Update Settings.hx

--- src/Settings.hx
+++ src/Settings.hx
@@ -36,7 +36,7 @@ class Settings extends FlxTransitionableState
 
 		FlxG.sound.cache(weatherMusic);
 
-		particlesEmitter = new FlxEmitter(FlxG.random.int(FlxG.width), 0, 200);
+		particlesEmitter = new FlxEmitter(FlxG.random.int(FlxG.width), 0);
 		particlesEmitter.loadParticles(AssetPaths.sprite('fallleaf'), 200);
 		particlesEmitter.alpha.set(0.5, 0.5);
 		add(particlesEmitter);
@@ -110,7 +110,7 @@ class Settings extends FlxTransitionableState
 
 		super.create();
 
-		particlesEmitter.start();
+		particlesEmitter.start(false, 0.01);
 	}
 
 	var siner:Int = 0;
