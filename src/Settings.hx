8d7f84788d66975afe3cb9d87fa040821ceca09a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-08-04 23:58:07 +0300

    Update Settings.hx

--- src/Settings.hx
+++ src/Settings.hx
@@ -40,6 +40,7 @@ class Settings extends FlxTransitionableState
 		particlesEmitter.loadParticles(AssetPaths.sprite('fallleaf'), 200);
 		particlesEmitter.alpha.set(0.5, 0.5);
 		particlesEmitter.scale.set(2, 2);
+		particlesEmitter.acceleration.set(0.02, 0.02);
 		particlesEmitter.velocity.set(150, 180);
 		add(particlesEmitter);
 
