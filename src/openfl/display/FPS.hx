59e56ec389ca9eb7405b1662e3e49931a39952d1
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-08-05 23:29:20 +0300

    Update FPS.hx

--- src/openfl/display/FPS.hx
+++ src/openfl/display/FPS.hx
@@ -36,9 +36,9 @@ class FPS extends TextField
 
 		#if mobile
 		defaultTextFormat = new TextFormat(Assets.getFont(AssetPaths.font('DTM-Sans')).fontName,
-			Std.int(18 * Math.min(FlxG.stage.stageWidth / FlxG.width, FlxG.stage.stageHeight / FlxG.height)), color);
+			Std.int(16 * Math.min(FlxG.stage.stageWidth / FlxG.width, FlxG.stage.stageHeight / FlxG.height)), color);
 		#else
-		defaultTextFormat = new TextFormat(Assets.getFont(AssetPaths.font('DTM-Sans')).fontName, 18, color);
+		defaultTextFormat = new TextFormat(Assets.getFont(AssetPaths.font('DTM-Sans')).fontName, 16, color);
 		#end
 	}
 
@@ -54,8 +54,8 @@ class FPS extends TextField
 		currentFPS = (times.length > Std.int(FlxG.stage.frameRate)) ? Std.int(FlxG.stage.frameRate) : times.length;
 
 		if (!showMemoryUsage)
-			text = currentFPS + ' FPS\n';
+			text = currentFPS + 'FPS\n';
 		else
-			text = currentFPS + ' FPS\n' + FlxStringUtil.formatBytes(System.totalMemory) + '\n';
+			text = currentFPS + 'FPS\n' + FlxStringUtil.formatBytes(System.totalMemory) + '\n';
 	}
 }
