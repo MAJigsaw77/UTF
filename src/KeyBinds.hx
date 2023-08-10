c9b530f33bdb2dc9d5de6d17a9385f52aed1e915
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-08-10 09:13:32 +0300

    Create KeyBinds.hx

---
+++ src/KeyBinds.hx
@@ -0,0 +1,10 @@
+package;
+
+import flixel.input.keyboard.FlxKey;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+
+class KeyBinds extends FlxSubState {}
