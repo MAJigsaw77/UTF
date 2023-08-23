8b6ec13c0c1d1229e583ce7bd79786e0654e22ed
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-08-23 18:04:23 +0300

    Create GameOver.hx

---
+++ source/states/GameOver.hx
@@ -0,0 +1,15 @@
+package states;
+
+import backend.AssetPaths;
+import objects.Writer;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+
+class GameOver extends FlxState
+{
+	override function create():Void
+	{
+		super.create();
+	}
+}
