9e986cc0e977357e9a61f27f77fb1a8812c051f9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-08-24 09:59:41 +0300

    Update GameOver.hx

--- source/states/GameOver.hx
+++ source/states/GameOver.hx
@@ -1,17 +1,28 @@
 package states;
 
 import backend.AssetPaths;
+import backend.Data;
 import objects.Writer;
+import flixel.tweens.FlxTween;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 
 class GameOver extends FlxState
 {
-	private var writer:Writer;
+	var bg:FlxSprite;
+	var writer:Writer;
 
 	override function create():Void
 	{
+		FlxG.sound.playMusic(AssetPaths.music('gameover'));
+
+		bg = new FlxSprite(0, 20, AssetPaths.sprite('gameoverbg'));
+		bg.alpha = 0;
+		bg.screenCenter(X);
+		bg.scrollFactor.set();
+		add(bg);
+
 		writer = new Writer(20, 256, 0, 32);
 
 		switch (FlxG.random.int(0, 4))
@@ -43,8 +54,28 @@ class GameOver extends FlxState
 				]);
 		}
 
+		writer.scrollFactor.set();
 		add(writer);
 
 		super.create();
+
+		FlxTween.tween(bg, {alpha: 1}, 1.5);
+	}
+
+	override function update(elapsed:Float):Void
+	{
+		if (FlxG.keys.checkStatus(Data.binds['confirm'], JUST_PRESSED) && writer == null && bg.alpha == 1)
+		{
+			FlxTween.tween(bg, {alpha: 1}, 1.5);
+			FlxG.sound.music.fadeOut(1.5);
+		}
+
+		super.update(elapsed);
+
+		if (writer != null && writer.finished)
+		{
+			remove(writer);
+			writer = null;
+		}
 	}
 }
