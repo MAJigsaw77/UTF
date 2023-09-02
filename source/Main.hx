c1694cc1676d8c8f6a583c5c7a6898c72182d774
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-02 13:45:53 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -184,12 +184,12 @@ class Main extends Sprite
 	private inline function onPreStateCreate(state:FlxState):Void
 	{
 		// Clear the loaded graphics if they are no longer in flixel cache...
-		for (key in Assets.cache.getBitmapKeys())
+		for (key in Assets.cache.bitmapData.keys())
 			if (!FlxG.bitmap.checkCache(key))
 				Assets.cache.removeBitmapData(key);
 
 		// Clear all the loaded sounds from the cache...
-		for (key in Assets.cache.getSoundKeys())
+		for (key in Assets.cache.sound.keys())
 			Assets.cache.removeSound(key);
 
 		#if MODS
