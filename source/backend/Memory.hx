669dc9507a99c940b6b1b1f04b9b3d566b467f6b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-05 09:48:16 +0300

    Create Memory.hx

---
+++ source/backend/Memory.hx
@@ -0,0 +1,38 @@
+package backend;
+
+#if windows
+@:headerInclude('windows.h')
+@:headerInclude('psapi.h')
+@:headerInclude('sysinfoapi.h')
+#end
+class Memory
+{
+	#if windows
+	@:functionCode('
+		unsigned long long info = 0;
+		GetPhysicallyInstalledSystemMemory(&info);
+		return (info / 1024);
+	')
+	@:noCompletion
+	private function getPhysicalInstalledMemory():Int
+	{
+		return 0;
+	}
+	#end
+
+	#if windows
+	@:functionCode('
+		PROCESS_MEMORY_COUNTERS info;
+
+		if (GetProcessMemoryInfo(GetCurrentProcess(), &info, sizeof(info)))
+			return info.WorkingSetSize;
+
+		return 0;
+	')
+	@:noCompletion
+	private function getProcessMemory():cpp.SizeT
+	{
+		return 0;
+	}
+	#end
+}
