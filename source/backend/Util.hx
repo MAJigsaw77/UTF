5d9412656d38d1a7c9ebab382c77bf5a4d2b76a1
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-09 08:14:05 +0300

    Update Util.hx

--- source/backend/Util.hx
+++ source/backend/Util.hx
@@ -8,4 +8,21 @@ class Util
 	{
 		return FlxG.random.float(0, Math.abs(a - b)) + Math.min(a, b);
 	}
+
+	public static function getWeather():Int
+	{
+		switch (Date.now().getMonth() + 1)
+		{
+			case 12 | 1 | 2: // Winter
+				return 1;
+			case 3 | 4 | 5: // Spring
+				return 2;
+			case 6 | 7 | 8: // Summer
+				return 3;
+			case 9 | 10 | 11: // Autumn
+				return 4;
+		}
+
+		return 0; // Default
+	}
 }
