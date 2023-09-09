ea4486c0493cf6ef105ea00d4c167da334a98ff3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-09 13:57:15 +0300

    Update Windows.hx

--- source/backend/Windows.hx
+++ source/backend/Windows.hx
@@ -1,21 +1,15 @@
 package backend;
 
 #if windows
-@:buildXml('
-<target id="haxe">
-	<lib name="dwmapi.lib" />
-</target>
-')
 @:headerInclude('windows.h')
-@:headerInclude('dwmapi.h')
 @:headerInclude('psapi.h')
-@:headerInclude('winuser.h')
 #end
 class Windows
 {
 	#if windows
 	@:functionCode('
 		PROCESS_MEMORY_COUNTERS pmc;
+
 		if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc)))
 			return pmc.WorkingSetSize;
 	')
@@ -23,18 +17,5 @@ class Windows
 	{
 		return 0;
 	}
-
-	@:functionCode('
-		HWND hwnd = GetActiveWindow();
-		DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &theme, sizeof(theme));
-		UpdateWindow(hwnd);
-	')
-	public static function setWindowTheme(theme:WindowColorTheme):Void {}
 	#end
-}
-
-enum abstract WindowColorTheme(UInt) from UInt to UInt
-{
-	var LIGHT = 0;
-	var DARK = 1;
 }
