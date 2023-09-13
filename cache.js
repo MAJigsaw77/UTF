4db76cbf26ed92eeb3ea5594fb106d07e89ffc8f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-13 19:40:05 +0300

    Create cache.js

---
+++ cache.js
@@ -0,0 +1,32 @@
+try
+{
+	const caches = await github.rest.actions.getActionsCacheList(
+	{
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+	})
+
+	if (typeof caches.data.actions_caches != null && caches.data.actions_caches.length > 0)
+	{
+		for (const cache of caches.data.actions_caches)
+		{
+			if (cache.key == 'cache-android-build')
+			{
+				console.log('Clearing ' + cache.key + '...')
+
+				await github.rest.actions.deleteActionsCacheById(
+				{
+					owner: context.repo.owner,
+					repo: context.repo.repo,
+					cache_id: cache.id,
+				})
+
+				console.log('Previous Cache Cleared!')
+			}
+		}
+	}
+}
+catch (error)
+{
+	console.log(error.message)
+}
