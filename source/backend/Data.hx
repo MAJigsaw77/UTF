975476303ea3074e7e47ea4dd5455cb11960d71a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-27 12:36:21 +0300

    Update Data.hx

--- source/backend/Data.hx
+++ source/backend/Data.hx
@@ -8,13 +8,14 @@ import openfl.Lib;
 
 class Data
 {
+	public static var settings:Map<String, Dynamic> = ['fps' => false, 'filter' => 'none'];
+
 	public static var filters:Map<String, BitmapFilter> = [
-		'Deuteranopia' => new ColorMatrixFilter([0.43, 0.72, -.15, 0, 0, 0.34, 0.57, 0.09, 0, 0, -.02, 0.03, 1, 0, 0, 0, 0, 0, 1, 0]),
-		'Protanopia' => new ColorMatrixFilter([0.20, 0.99, -.19, 0, 0, 0.16, 0.79, 0.04, 0, 0, 0.01, -.01, 1, 0, 0, 0, 0, 0, 1, 0]),
-		'Tritanopia' => new ColorMatrixFilter([0.97, 0.11, -.08, 0, 0, 0.02, 0.82, 0.16, 0, 0, 0.06, 0.88, 0.18, 0, 0, 0, 0, 0, 1, 0])
+		'deuteranopia' => new ColorMatrixFilter([0.43, 0.72, -0.15, 0, 0, 0.34, 0.57, 0.09, 0, 0, -0.02, 0.03, 1, 0, 0, 0, 0, 0, 1, 0]),
+		'protanopia' => new ColorMatrixFilter([0.20, 0.99, -0.19, 0, 0, 0.16, 0.79, 0.04, 0, 0, 0.01, -0.01, 1, 0, 0, 0, 0, 0, 1, 0]),
+		'pritanopia' => new ColorMatrixFilter([0.97, 0.11, -0.08, 0, 0, 0.02, 0.82, 0.16, 0, 0, 0.06, 0.88, 0.18, 0, 0, 0, 0, 0, 1, 0])
 	];
 
-	public static var settings:Map<String, Dynamic> = ['fps' => false];
 	public static var binds:Map<String, FlxKey> = ['confirm' => Z, 'cancel' => X, 'menu' => C];
 
 	public static function save():Void
@@ -41,5 +42,19 @@ class Data
 		}
 
 		save.destroy();
+	}
+
+	public static function loadFilters():Void
+	{
+		var filters:Array<BitmapFilter> = [];
+
+		// If the filter got modified or is `none` it won't set anything.
+		if (Data.settings.get('filter') != null && Data.settings.get('filter') != 'none')
+		{
+			if (Data.filters.exists(Data.settings.get('filter'))
+			    filters[0] = Data.filters.get(Data.settings.get('filter'));
+		}
+
+		FlxG.game.setFilters(filters);
 	}
 }
