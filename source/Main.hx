f40a2c533a94716e71642297cce7c32a33b3aa8d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-10-19 13:04:46 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -71,11 +71,18 @@ class Main extends Sprite
 		#end
 
 		fps = new FPS(10, 10, FlxColor.RED);
+
 		#if (mobile || switch)
 		fps.scaleX = fps.scaleY = Math.min(FlxG.stage.stageWidth / FlxG.width, FlxG.stage.stageHeight / FlxG.height);
 		#end
+
 		fps.visible = Data.settings['fps'];
+
+		#if !mobile
 		addChild(fps);
+		#else
+		FlxG.game.addChild(fps);
+		#end
 	}
 
 	private inline function onUncaughtError(event:UncaughtErrorEvent):Void
