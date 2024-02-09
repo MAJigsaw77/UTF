3b3147648fc505e905addbc52ca902eaedea6f4a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-02-09 21:34:35 +0200

    Update GameOver.hx

--- source/states/GameOver.hx
+++ source/states/GameOver.hx
@@ -40,7 +40,7 @@ class GameOver extends FlxState
 			{
 				new FlxTimer().start(1, function(tmr:FlxTimer)
 				{
-					final typer:Typer = new Typer({name: 'DTM-Mono', size: 32}, {name: 'txt2', volume: 0.86}, 6, 0);
+					final typer:Typer = new Typer({name: 'DTM-Mono', size: 32}, {name: 'txt2', volume: 0.86}, 6, 2);
 
 					switch (FlxG.random.int(0, 4))
 					{
