d2ae51a0fbc1e74bdeb135455d45e118fa3c678d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-02-15 20:37:57 +0200

    Update Room.hx

--- source/backend/Room.hx
+++ source/backend/Room.hx
@@ -11,6 +11,8 @@ class Room
 {
 	public static var data(default, null):Map<Int, {file:String, content:Xml}> = [];
 
+	private static final directory:String = 'assets/data/rooms';
+
 	public static function reloadFiles():Void
 	{
 		if (data != null && Lambda.count(data) > 0)
@@ -18,7 +20,7 @@ class Room
 
 		final files:Array<String> = Assets.list(TEXT).filter(function(file:String):Bool
 		{
-			return Path.directory(file) == 'assets/data/rooms' && Path.extension(file) == 'xml';
+			return Path.directory(file) == directory && Path.extension(file) == 'xml';
 		});
 
 		files.sort(function(a:String, b:String):Int
@@ -33,9 +35,12 @@ class Room
 			if (parsed.get('id') == null || parsed.get('id').length <= 0)
 				continue;
 
-			data.set(Std.parseInt(parsed.get('id')), {file: new Path(file).file, content: parsed});
-		}
+			final id:Int = Std.parseInt(parsed.get('id'));
 
-		FlxG.log.notice(data);
+			if (data.exists(id))
+				FlxG.log.notice('Overwriting room $id.');
+
+			data.set(id, {file: Path.withoutExtension(file.replace(Path.addTrailingSlash(directory), '')), content: parsed});
+		}
 	}
 }
