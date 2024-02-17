b74349191697352f426b826c90d3399c2834643a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-02-17 22:02:19 +0000

    We only need the script file

--- source/objects/room/Object.hx
+++ source/objects/room/Object.hx
@@ -3,20 +3,10 @@ package objects.room;
 import backend.AssetPaths;
 import backend.Script;
 import flixel.FlxSprite;
-import haxe.Json;
-import openfl.utils.Assets;
-
-typedef ObjectData =
-{
-	solid:Bool,
-	active:Bool,
-	?hitbox:Array<Float>
-}
 
 class Object extends FlxSprite
 {
 	public var name(default, null):String;
-	public var data(default, null):ObjectData;
 	public var script(default, null):Script;
 
 	public function new(x:Float = 0, y:Float = 0, name:String):Void
@@ -24,16 +14,6 @@ class Object extends FlxSprite
 		this.name = name;
 
 		super(x, y);
-
-		immovable = true;
-
-		if (Assets.exists(AssetPaths.data('objects/$name')))
-			data = Json.parse(Assets.getText(AssetPaths.data('objects/$name')));
-		else
-			data = {solid: false, active: false};
-
-		solid = data.solid;
-		active = data.active;
 
 		script = new Script();
 		script.set('this', this);
