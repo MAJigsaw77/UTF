55843bb7a5fdf5a551e86301bb954076d2ab9de0
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-03-03 18:12:15 +0200

    Update Typer.hx

--- source/objects/dialogue/Typer.hx
+++ source/objects/dialogue/Typer.hx
@@ -18,12 +18,14 @@ class Typer
 	public var sound:TyperSound;
 	public var speed:Float;
 	public var spacing:Null<Float>;
+	public var face:String;
 
-	public function new(font:TyperFont, sound:TyperSound, speed:Float, ?spacing:Null<Float>):Void
+	public function new(font:TyperFont, sound:TyperSound, speed:Float, ?spacing:Null<Float>, ?portrait:String):Void
 	{
 		this.font = font;
 		this.sound = sound;
 		this.speed = speed;
 		this.spacing = spacing;
+		this.face = face;
 	}
 }
