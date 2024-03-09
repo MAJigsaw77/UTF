8e98e611c34e7e243165f535934e70e4a841c303
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-03-09 23:12:08 +0200

    Update Writer.hx

--- source/objects/dialogue/Writer.hx
+++ source/objects/dialogue/Writer.hx
@@ -16,8 +16,10 @@ typedef DialogueData = {
 
 class Writer extends TypeText
 {
+	public var skippable:Bool = true;
 	public var finishCallback:Void->Void = null;
 
+	var done:Bool = false;
 	var list:Array<DialogueData> = [];
 	var page:Int = 0;
 
@@ -28,7 +30,7 @@ class Writer extends TypeText
 
 	public function startDialogue(list:Array<DialogueData>):Void
 	{
-		this.list = list ??= [{ typer: new Typer({ name: 'DTM-Mono', size: 32 }, { name: 'txt2', volume: 0.86 }, 4), text: 'Error!' }];
+		this.list = list ?? [{ typer: new Typer({ name: 'DTM-Mono', size: 32 }, { name: 'txt1', volume: 1 }, 2), text: 'Error!' }];
 
 		page = 0;
 
@@ -39,7 +41,7 @@ class Writer extends TypeText
 	public function changeDialogue(dialogue:DialogueData):Void
 	{
 		if (dialogue == null)
-			dialogue = {typer: new Typer({name: 'DTM-Mono', size: 32}, {name: 'txt2', volume: 0.86}, 4), text: 'Error!'};
+			dialogue = {typer: new Typer({name: 'DTM-Mono', size: 32}, {name: 'txt1', volume: 1}, 2), text: 'Error!'};
 
 		sounds = [FlxG.sound.load(AssetPaths.sound(dialogue.typer.sound.name), dialogue.typer.sound.volume)];
 
@@ -52,6 +54,8 @@ class Writer extends TypeText
 		if (dialogue.typer.spacing != null && letterSpacing != dialogue.typer.spacing)
 			letterSpacing = dialogue.typer.spacing;
 
+		done = false;
+
 		start(dialogue.text, 1 / (dialogue.typer.speed * 10));
 	}
 
@@ -59,23 +63,23 @@ class Writer extends TypeText
 	{
 		if (Controls.instance.justPressed('confirm'))
 		{
-			if (!finished)
-			{
+			if (Controls.instance.justPressed('cancel') && !finished && skippable)
 				skip();
-
-				return;
-			}
-
-			if (page < list.indexOf(list.last()))
+			else if (finished && !done)
 			{
-				page++;
+				if (page < list.indexOf(list.last()))
+				{
+					page++;
 
-				changeDialogue(list[page]);
-			}
-			else if (page == list.indexOf(list.last()))
-			{
-				if (finishCallback != null)
-					finishCallback();
+					changeDialogue(list[page]);
+				}
+				else if (page == list.indexOf(list.last()))
+				{
+					if (finishCallback != null)
+						finishCallback();
+
+					done = true;
+				}
 			}
 		}
 
