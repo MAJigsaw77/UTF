91729cfe29f5d5ccc933b31cc544b5f156aeb7bd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-03-10 02:08:28 +0200

    Update Macros.hx

--- source/backend/Macros.hx
+++ source/backend/Macros.hx
@@ -51,32 +51,35 @@ class Macros
 	public static macro function includePackages():Expr
 	{
 		#if !display
-		Compiler.include("flixel.addons.api");
-		Compiler.include("flixel.addons.display");
-		Compiler.include("flixel.addons.effects");
-		Compiler.include("flixel.addons.plugin");
-		Compiler.include("flixel.addons.text");
-		Compiler.include("flixel.addons.tile");
-		Compiler.include("flixel.addons.transition");
-		Compiler.include("flixel.addons.ui");
-		Compiler.include("flixel.addons.util");
+		Compiler.include('flixel.addons.api');
+		Compiler.include('flixel.addons.display');
+		Compiler.include('flixel.addons.effects');
+		Compiler.include('flixel.addons.plugin');
+		Compiler.include('flixel.addons.text');
+		Compiler.include('flixel.addons.tile');
+		Compiler.include('flixel.addons.transition');
+		Compiler.include('flixel.addons.ui');
+		Compiler.include('flixel.addons.util');
 
-		Compiler.include("flixel.animation");
-		Compiler.include("flixel.effects");
-		Compiler.include("flixel.graphics");
-		Compiler.include("flixel.group");
-		Compiler.include("flixel.input");
-		Compiler.include("flixel.math");
-		Compiler.include("flixel.path");
-		Compiler.include("flixel.sound");
-		Compiler.include("flixel.system");
-		Compiler.include("flixel.text");
-		Compiler.include("flixel.tile");
-		Compiler.include("flixel.tweens");
-		Compiler.include("flixel.ui");
-		Compiler.include("flixel.util");
+		Compiler.include('flixel.animation');
+		Compiler.include('flixel.effects');
+		Compiler.include('flixel.graphics');
+		Compiler.include('flixel.group');
+		Compiler.include('flixel.input');
+		Compiler.include('flixel.math');
+		Compiler.include('flixel.path');
+		Compiler.include('flixel.sound');
+		Compiler.include('flixel.system');
+		Compiler.include('flixel.text');
+		Compiler.include('flixel.tile');
+		Compiler.include('flixel.tweens');
+		Compiler.include('flixel.ui');
+		Compiler.include('flixel.util');
 
-		Compiler.include('haxe');
+		Compiler.include('haxe.crypto');
+		Compiler.include('haxe.display');
+		Compiler.include('haxe.exceptions');
+		Compiler.include('haxe.extern');
 		#end
 
 		return macro $v{null};
