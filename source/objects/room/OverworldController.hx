0faef7a099b9ae86956c2df02fa89df3ae30144c
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-03-12 10:37:49 +0200

    Create OverworldController.hx

---
+++ source/objects/room/OverworldController.hx
@@ -0,0 +1,17 @@
+package objects.battle;
+
+import backend.AssetPaths;
+import flixel.group.FlxSpriteGroup;
+
+class OverworldController extends FlxSpriteGroup
+{
+	public function new(x:Float = 0, y:Float = 0):Void
+	{
+		super(x, y);
+	}
+
+	public override function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
