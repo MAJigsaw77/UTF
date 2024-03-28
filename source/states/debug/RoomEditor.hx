eb08c5745feb2bc13fd859022c3ab5cd5c344812
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-03-28 17:46:31 +0200

    Create RoomEditor.hx

---
+++ source/states/debug/RoomEditor.hx
@@ -0,0 +1,13 @@
+package states.debug;
+
+import flixel.addons.transition.FlxTransitionableState;
+import haxe.ui.containers.HBox;
+import haxe.ui.containers.VBox;
+
+class RoomEditor extends FlxTransitionableState
+{
+	override function create():Void
+	{
+		super.create();
+	}
+}
